3eeeed95424159c586af41a1b887f810079ce75e
Stanisław Karkosz <34490120+stakar@users.noreply.github.com>
2019-12-16 11:11:50 +0100

    Update script_ptp.m
    Fixed problem with time window

--- script_ptp.m
+++ script_ptp.m
@@ -6,12 +6,23 @@ if ~iscell(FileName)
     FileName={FileName};
 end
 
+LowerTimeWindow = 150;
+group = '0';
 
-
-A = readtable('4_SN_lat.txt','ReadVariableNames',true);
+A = readtable([group '_SN_lat.txt'],'ReadVariableNames',true);
 %Na początku musi dostać tabelę z latencjami dla pozytywnych peaków p300
 
 placeholder = zeros(height(A),width(A));
+disp('Default settings of importing set:')
+disp(['Length of set: ' num2str(length(FileName))])
+disp(['Length of A: ' num2str(height(A))])
+
+if (length(FileName) == height(A))
+    disp('Fixed settings of importing set:')
+    A = readtable([group '_SN_lat.txt'],'ReadVariableNames',false);
+    disp(['Length of set: ' num2str(length(FileName))])
+    disp(['Length of A: ' num2str(height(A))])
+end
 
 for i = 1:length(FileName)
     
@@ -27,15 +38,21 @@ for i = 1:length(FileName)
             disp(chan)
             disp(bin)
             disp(latency)
-            measurement = pop_geterpvalues(ERP,[latency-100 latency],bin,chan,...
+            if (latency == LowerTimeWindow)
+                lower = (latency-100);
+            else 
+                lower = LowerTimeWindow;
+            end
+            measurement = pop_geterpvalues(ERP,[lower latency],bin,chan,...
              'Baseline', [ -100 0], 'Binlabel', 'on', 'FileFormat', 'wide', 'Filename', 'test.txt',...
-             'Fracreplace', 'NaN', 'InterpFactor',  1, 'Measure', 'peakampbl', 'Neighborhood',  3, 'PeakOnset',  1, 'Peakpolarity', 'positive',...
+             'Fracreplace', 'NaN', 'InterpFactor',  1, 'Measure', 'peakampbl', 'Neighborhood',  3,...
+             'PeakOnset',  1, 'Peakpolarity', 'positive',...
              'Peakreplace', 'absolute', 'Resolution',  3, 'SendtoWorkspace', 'on' );
             placeholder(i,column) = ERP_MEASURES;
         end
     end
 end
-WriteMatrix2Text(placeholder,'4_SN_amp_ptp.txt')
+WriteMatrix2Text(placeholder,[group '_SN_amp_ptp.txt'])
 
 
 for i = 1:length(FileName)
@@ -52,7 +69,12 @@ for i = 1:length(FileName)
             disp(chan)
             disp(bin)
             disp(latency)
-            measurement = pop_geterpvalues(ERP,[latency-100 latency],bin,chan,...
+            if (latency == LowerTimeWindow)
+                lower = (latency-100);
+            else 
+                lower = LowerTimeWindow;
+            end
+            measurement = pop_geterpvalues(ERP,[lower latency],bin,chan,...
              'Baseline', [ -100 0], 'Binlabel', 'on', 'FileFormat', 'wide', 'Filename', 'test.txt',...
              'Fracreplace', 'NaN', 'InterpFactor',  1, 'Measure', 'peaklatbl', 'Neighborhood',  3, 'PeakOnset',  1, 'Peakpolarity', 'positive',...
              'Peakreplace', 'absolute', 'Resolution',  3, 'SendtoWorkspace', 'on' );
@@ -60,4 +82,4 @@ for i = 1:length(FileName)
         end
     end
 end
-WriteMatrix2Text(placeholder,'4_SN_lat_ptp.txt')
+WriteMatrix2Text(placeholder,[group '_SN_lat_ptp.txt'])
